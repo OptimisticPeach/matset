e9af91daf2a7163a6e43eb2eedc8c7691dc8d425
Patrik Buhring <patrikbuhring@gmail.com>
2025-02-21 09:51:08 -0500

    commit

--- src/parse.rs
+++ src/parse.rs
@@ -38,30 +38,24 @@ pub enum ExprAstNode {
     },
 }
 
-pub fn parse<'a>(
-    typst_ast: &TypstAst<'a>,
-    symbols: &mut HashSet<String>,
-) -> Result<Option<ExprAstNode>, String> {
+pub fn parse<'a>(typst_ast: &TypstAst<'a>) -> Result<Option<ExprAstNode>, String> {
     match typst_ast {
-        TypstAst::Equation { body, .. } => parse(body, symbols),
-        TypstAst::Sequence { children } => parse_sequence(children, symbols),
+        TypstAst::Equation { body, .. } => parse(body),
+        TypstAst::Sequence { children } => todo!(),
+        TypstAst::Symbol { text } => Ok(Some(
+            Value::parse(text)
+                .map(ExprAstNode::Constant)
+                .unwrap_or_else(|| ExprAstNode::Name(text.to_string())),
+        )),
         TypstAst::Text { text } => Ok(Some(
             Value::parse(text)
                 .map(ExprAstNode::Constant)
-                .unwrap_or_else(|| {
-                    symbols.insert(text.to_string());
-
-                    ExprAstNode::Name(text.to_string())
-                }),
+                .unwrap_or_else(|| ExprAstNode::Name(text.to_string())),
         )),
         TypstAst::Space => Ok(None),
         TypstAst::Frac { num, denom } => Ok(Some(ExprAstNode::Fraction {
-            num: parse(num, symbols)?
-                .ok_or("Fractions need a numerator!")?
-                .into(),
-            denom: parse(denom, symbols)?
-                .ok_or("Fractions need a denominator!")?
-                .into(),
+            num: parse(num)?.ok_or("Fractions need a numerator!")?.into(),
+            denom: parse(denom)?.ok_or("Fractions need a denominator!")?.into(),
         })),
         TypstAst::Attach { base, t, tr, b, .. } => {
             if let Some(b) = b {
@@ -80,11 +74,9 @@ pub fn parse<'a>(
                 {
                     let new_name = format!("{base_text}_{text}");
 
-                    symbols.insert(new_name.clone());
-
                     ExprAstNode::Name(new_name)
                 } else {
-                    parse(base, symbols)?.ok_or("Exponents need bases!")?
+                    parse(base)?.ok_or("Exponents need bases!")?
                 };
 
             if let Some(tr) = tr {
@@ -99,7 +91,7 @@ pub fn parse<'a>(
             }
 
             if let Some(t) = t {
-                let superscript = parse(t, symbols)?.ok_or("Cannot have empty superscript!")?;
+                let superscript = parse(t)?.ok_or("Cannot have empty superscript!")?;
 
                 base = ExprAstNode::Power {
                     base: base.into(),
@@ -109,27 +101,49 @@ pub fn parse<'a>(
 
             Ok(Some(base))
         }
-        TypstAst::LeftRight { body } => todo!(),
+        TypstAst::LeftRight { body } => {
+            let TypstAst::Sequence { children } = &**body else {
+                Err("Expected Sequence in left/right node")?
+            };
+
+            if children.len() < 3 {
+                Err("Left/right node has fewer than three elements, are your parentheses empty?")?
+            }
+
+            // let [left, inner @ .., right] = &children[..];
+
+            // let (TypstAst::Text { text: left } | TypstAst::Symbol { text: left }) = left else {
+            //     Err("Unexpected left element in left/right node")?
+            // };
+
+            // let (TypstAst::Text { text: right } | TypstAst::Symbol { text: right }) = right else {
+            //     Err("Unexpected right element in left/right node")?
+            // };
+
+            // if left != "(" || right != ")" {
+            //     Err("Unsupported parentheses!")?
+            // }
+            //
+            todo!()
+        }
         TypstAst::Primes { .. } => Err("Free-floating primes don't make sense!")?,
         TypstAst::Root { index, radicand } => Ok(Some(ExprAstNode::Radical {
             index: index
                 .as_ref()
-                .map(|x| parse(x, symbols))
+                .map(|x| parse(x))
                 .transpose()?
                 .flatten()
                 .map(Into::into),
-            radicand: parse(radicand, symbols)?
-                .ok_or("Radicals need a radicand!")?
-                .into(),
+            radicand: parse(radicand)?.ok_or("Radicals need a radicand!")?.into(),
         })),
         TypstAst::Binomial { upper, lower } => Ok(Some(ExprAstNode::Binomial {
-            upper: parse(upper, symbols)?
+            upper: parse(upper)?
                 .ok_or("Binomials need an upper element!")?
                 .into(),
             lower: {
                 let result = lower
                     .iter()
-                    .map(|x| parse(x, symbols))
+                    .map(|x| parse(x))
                     .filter(|x| !matches!(x, Ok(None)))
                     .collect::<Result<Option<Vec<ExprAstNode>>, String>>()?
                     .unwrap();
@@ -142,10 +156,4 @@ pub fn parse<'a>(
         })),
         TypstAst::Operator { .. } => unimplemented!(),
     }
-}
-
-fn parse_sequence(
-    seq: &[TypstAst<'_>],
-    symbols: &mut HashSet<String>,
-) -> Result<Option<ExprAstNode>, String> {
 }
--- src/typst_ast.rs
+++ src/typst_ast.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Write};
+
 use serde::{Deserialize, Serialize};
 
 type BAst<'a> = Box<TypstAst<'a>>;
@@ -51,15 +53,106 @@ pub enum TypstAst<'a> {
 
     #[serde(rename = "op")]
     Operator { text: BAst<'a>, limits: bool },
+
+    #[serde(rename = "symbol")]
+    Symbol { text: &'a str },
+}
+
+impl<'a> Display for TypstAst<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.format_into(f)
+    }
+}
+
+impl<'a> TypstAst<'a> {
+    fn format_into<W: Write>(&self, out: &mut W) -> std::fmt::Result {
+        match self {
+            TypstAst::Equation { body, .. } => body.format_into(out),
+            TypstAst::Sequence { children } => children
+                .iter()
+                .map(|x| x.format_into(out))
+                .collect::<std::fmt::Result>(),
+            TypstAst::Text { text } => write!(out, "{text}"),
+            TypstAst::Space => write!(out, " "),
+            TypstAst::Frac { num, denom } => write!(out, "frac({num}, {denom})"),
+            TypstAst::Attach {
+                base,
+                t,
+                tr,
+                br,
+                b,
+                bl,
+                tl,
+            } => {
+                assert!(br.is_none() && bl.is_none() && tl.is_none());
+
+                let write_subscript = |out: &mut W| -> std::fmt::Result {
+                    if let Some(b) = b {
+                        let (TypstAst::Text { text } | TypstAst::Symbol { text }) = &**b else {
+                            panic!("Unsupported subscript!");
+                        };
+
+                        write!(out, "subscript({base}, {text})")
+                    } else {
+                        write!(out, "{base}")
+                    }
+                };
+
+                let write_base = |out: &mut W| -> std::fmt::Result {
+                    if let Some(tr) = tr {
+                        let TypstAst::Primes { count } = &**tr else {
+                            panic!("Unexpected value in tr of attach!");
+                        };
+
+                        write!(out, "primes(")?;
+                        write_subscript(out)?;
+                        write!(out, ", {count})")
+                    } else {
+                        write_subscript(out)
+                    }
+                };
+
+                if let Some(t) = t {
+                    write!(out, "pow(")?;
+                    write_base(out)?;
+                    write!(out, ", {t})")
+                } else {
+                    write_base(out)
+                }
+            }
+            TypstAst::LeftRight { body } => body.format_into(out),
+            TypstAst::Primes { .. } => panic!("Free-floating primes are not supported!"),
+            TypstAst::Root { index, radicand } => {
+                if let Some(index) = index {
+                    write!(out, "nthroot({radicand}, {index})")
+                } else {
+                    write!(out, "sqrt({radicand})")
+                }
+            }
+            TypstAst::Binomial { upper, lower } => {
+                write!(out, "binom({upper}; ")?;
+                assert!(lower.len() > 0, "Binomials need a lower part!");
+                for elem in lower[0..lower.len() - 1].iter() {
+                    write!(out, "{elem}, ")?;
+                }
+
+                write!(out, "{})", lower.last().unwrap())
+            }
+            TypstAst::Operator { text, .. } => write!(out, "{text}"),
+            TypstAst::Symbol { text } => write!(out, "{text}"),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::TypstAst;
 
+    static TESTING_SRC: &'static str = include_str!("./typst_ast_test_input.json");
+
     #[test]
     fn parses_correctly() {
-        let src = include_str!("./typst_ast_test_input.json");
+        let src = TESTING_SRC;
         let parsed = serde_json::from_str::<TypstAst>(src).unwrap();
 
         use TypstAst::*;
@@ -69,11 +162,11 @@ mod tests {
             body: Sequence {
                 children: vec![
                     Attach {
-                        base: Text { text: "x" }.into(),
+                        base: Symbol { text: "x" }.into(),
                         t: Some(
                             Binomial {
                                 upper: Text { text: "a" }.into(),
-                                lower: vec![Text { text: "b" }],
+                                lower: vec![Symbol { text: "b" }],
                             }
                             .into(),
                         ),
@@ -84,7 +177,7 @@ mod tests {
                         tl: None,
                     },
                     Space,
-                    Text { text: "=" },
+                    Symbol { text: "=" },
                     Space,
                     Attach {
                         base: Frac {
@@ -101,9 +194,9 @@ mod tests {
                             denom: LeftRight {
                                 body: Sequence {
                                     children: vec![
-                                        Text { text: "‖" },
-                                        Text { text: "y" },
-                                        Text { text: "‖" },
+                                        Symbol { text: "‖" },
+                                        Symbol { text: "y" },
+                                        Symbol { text: "‖" },
                                     ],
                                 }
                                 .into(),
@@ -122,19 +215,37 @@ mod tests {
                     LeftRight {
                         body: Sequence {
                             children: vec![
-                                Text { text: "⟨" },
+                                Symbol { text: "⟨" },
                                 Space,
                                 Root {
-                                    index: Some(Text { text: "α" }.into()),
+                                    index: Some(Symbol { text: "α" }.into()),
                                     radicand: Text { text: "12" }.into(),
                                 },
-                                Text { text: "," },
+                                Symbol { text: "," },
                                 Root {
                                     index: None,
-                                    radicand: Text { text: "x" }.into(),
+                                    radicand: Sequence {
+                                        children: vec![
+                                            Operator {
+                                                text: Text { text: "sin" }.into(),
+                                                limits: false,
+                                            },
+                                            LeftRight {
+                                                body: Sequence {
+                                                    children: vec![
+                                                        Symbol { text: "(" },
+                                                        Symbol { text: "x" },
+                                                        Symbol { text: ")" },
+                                                    ],
+                                                }
+                                                .into(),
+                                            },
+                                        ],
+                                    }
+                                    .into(),
                                 },
                                 Space,
-                                Text { text: "⟩" },
+                                Symbol { text: "⟩" },
                             ],
                         }
                         .into(),
@@ -145,5 +256,16 @@ mod tests {
         };
 
         assert_eq!(parsed, correct);
+    }
+
+    #[test]
+    fn stringifies_correctly() {
+        let src = TESTING_SRC;
+        let parsed = serde_json::from_str::<TypstAst>(src).unwrap();
+
+        assert_eq!(
+            format!("{parsed}"),
+            "pow(x, binom(a; b)) = frac(primes(54, 2), ‖y‖) ⟨ nthroot(12, α),sqrt(sin(x)) ⟩"
+        );
     }
 }
